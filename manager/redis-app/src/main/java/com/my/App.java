4b9c2844cb348f9d0a55b0542fd20fb4b1d854a1
liuliang14 <liuliang14@guazi.com>
2022-04-02 17:02:48 +0800

    tijao

--- manager/redis-app/src/main/java/com/my/App.java
+++ manager/redis-app/src/main/java/com/my/App.java
@@ -4,6 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  * Hello world!
  */
@@ -24,6 +29,19 @@ public class App {
     }
 
     @Test
+    public void testHash() {
+        Jedis jedis = new Jedis("10.16.208.93", 7103);
+        Card card = new Card();
+        card.setId(123455);
+        card.setPwd("2222");
+        byte[] bytes = toByteArray(card);
+
+        Long dental_card = jedis.hset("gujzi_card".getBytes(),"test".getBytes(), bytes);
+        System.out.println(dental_card);
+
+    }
+
+    @Test
     public void testListPop() {
         Jedis jedis = new Jedis("192.168.77.135", 6379);
         Card card = new Card();
@@ -35,7 +53,23 @@ public class App {
 
     }
 
-    static class Card {
+    public byte[] toByteArray (Object obj) {
+        byte[] bytes = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = bos.toByteArray ();
+            oos.close();
+            bos.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return bytes;
+    }
+
+    static class Card implements Serializable {
 
         private int id;
         private String pwd;
