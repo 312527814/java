a7191bdff830f822c2632c310eedc8b4a7fa2936
liuiang <liuliang@gstanzer.com>
2024-07-31 11:49:24 +0800

    log

--- manager/bigdata-hadoop/src/main/java/com/my/mapreduce/wc/MyWordCount.java
+++ manager/bigdata-hadoop/src/main/java/com/my/mapreduce/wc/MyWordCount.java
@@ -8,6 +8,8 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class MyWordCount {
 
@@ -18,8 +20,12 @@ public class MyWordCount {
     //  args :   2类参数  genericOptions   commandOptions
     //  人你有复杂度：  自己分析 args数组
     //
+    private static final Logger logger = Logger.getLogger(MyMapper.class);
     public static void main(String[] args) throws Exception {
 
+        BasicConfigurator.configure();
+
+//        logger.info("ddddddd");
         Configuration conf = new Configuration(true);
 
 //        conf.setLong("mapreduce.input.fileinputformat.split.minsize", Integer.MAX_VALUE);
@@ -28,10 +34,14 @@ public class MyWordCount {
         String[] othargs = parser.getRemainingArgs();
 
         //让框架知道是windows异构平台运行
-//        conf.set("mapreduce.app-submission.cross-platform","true");
+        conf.set("mapreduce.app-submission.cross-platform","true");
 
-//        conf.set("mapreduce.framework.name","local");
+        //本地运行模式，不会把jar发到datanode
+        conf.set("mapreduce.framework.name","local");
 //        System.out.println(conf.get("mapreduce.framework.name"));
+        // 异构平台运行，如果运行的是集群模式，需要将单独打包 将jar发到datanode
+//        job.setJar("F:\\xinjianwenjianjia\\java-practice\\manager\\bigdata-hadoop\\target\\bigdata-hadoop-1.0-SNAPSHOT.jar");
+
 
         Job job = Job.getInstance(conf);
 
@@ -44,7 +54,6 @@ public class MyWordCount {
 
 
 
-//        job.setJar("C:\\Users\\admin\\IdeaProjects\\msbhadoop\\target\\hadoop-hdfs-1.0-0.1.jar");
         //必须必须写的
         job.setJarByClass(MyWordCount.class);
 
